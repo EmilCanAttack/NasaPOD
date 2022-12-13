9e15431c5b2e70f637a8e50aed66570afcc99e5c
EmilCanAttack <90251019+EmilCanAttack@users.noreply.github.com>
2022-12-13 12:57:46 +0100

    2. commit

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.get('/nasaPOD', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Listening on localhost:3000');
+app.listen(8080, () => {
+  console.log('Listening on localhost:8080');
 });
