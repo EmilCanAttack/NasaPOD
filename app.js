95ec32c58235c0a6b019308b2961231c2b4026a3
EmilCanAttack <90251019+EmilCanAttack@users.noreply.github.com>
2022-12-13 12:10:26 +0100

    Første commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+
+app.get('/nasaPOD', (req, res) => {
+  // NASA POD API endpoint
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=pj8RlyhXoZqX0KIcBXydsH0laDHCABm0IaexhvzL';
+
+  request(url, (error, response, body) => {
+    // parse the JSON response
+    const data = JSON.parse(body);
+    // extract the URL, date and description of the picture from the response
+    const imgUrl = data.url;
+    const description = data.explanation;
+    const date = data.date;
+    // create an HTML page with the img and text
+    const html = `
+    <style>
+    body {
+      text-align: center;
+    }
+    img {
+        width: 700px;
+    }
+    p {
+      text-align: center;
+    }
+  </style>
+  <h1>NASA PICTURE OF THE DAY</h1>
+  <h2>${date}</h2>
+  <img src="${imgUrl}" />
+  <h2>DESCRIPTION</h2>
+  <p>${description}</p>
+    `;
+    // send the HTML page as a response
+    res.send(html);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on localhost:3000');
+});
