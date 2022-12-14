a8a714cd45dae14e73f57673a6fe47bf111dffe8
EmilCanAttack <90251019+EmilCanAttack@users.noreply.github.com>
2022-12-14 15:18:18 +0100

    Ziya side tilføjet

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ app.get('/', (req, res) => {
   const url = 'https://api.nasa.gov/planetary/apod?api_key=pj8RlyhXoZqX0KIcBXydsH0laDHCABm0IaexhvzL';
 
   request(url, (error, response, body) => {
-    // parse the JSON response
+    // parse JSON response til JS syntax
     const data = JSON.parse(body);
-    // extract the URL, title, date and description of the picture from the response
+    // udtræk URL, titel, dato og beskrivelse af billedet fra response
     const imgUrl = data.url;
     const description = data.explanation;
     const date = data.date;
     const title = data.title;
-    // create an HTML page with the img and text
+    // Lav en HTML side med img og text
     const html = `
     <style>
     body {
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
       color: white;
     }
     img {
-        width: 850px;
+        width: 600px;
     }
     p {
       text-align: center;
@@ -36,11 +36,43 @@ app.get('/', (req, res) => {
   <img src="${imgUrl}" />
   <h2>DESCRIPTION</h2>
   <p>${description}</p>
+  <br>
+  <button onclick="location.href='/ziya'">Click here to see ZIYA ATAN</button>
     `;
-    // send the HTML page as a response
+    // send html as response
     res.send(html);
   });
 });
+
+// ZIYA ATAN HJEMMESIDE 
+app.get('/ziya', (req, res) => {
+  // HTML HJEMMESIDE DEDIKERET TIL MIN FAR
+  const imgUrl = "https://i.ibb.co/Ry1bLDh/ziyanmark.jpg"
+  const html = `
+  <style>
+    body {
+      text-align: center;
+      background-color: black;
+    }
+    img {
+      width: 600px;
+    }
+    h1 {
+      font-size: 72px;
+      letter-spacing: 5px;
+      background: linear-gradient(to right, red, white);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+  </style>
+  <h1>ZIYA ATAN</h1>
+  <img src="${imgUrl}" />
+  `;
+
+  // send the HTML page as a response
+  res.send(html);
+});
+
 
 app.listen(8080, () => {
   console.log('Listening on localhost:8080');
